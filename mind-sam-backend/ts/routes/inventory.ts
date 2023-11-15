820022ce81d322d4a8739d16430aa9a48f90f127
612943810 <raiyanrofiquizzaman@gmail.com>
2023-11-14 23:07:43 -0500

    This update adds more routes.

--- mind-sam-backend/ts/routes/inventory.ts
+++ mind-sam-backend/ts/routes/inventory.ts
@@ -3,3 +3,5 @@ let inController=require('../controller/InventoryController')
 export let inRoute=Router();
 inRoute.post("/postinventory",inController.postInventory)
 inRoute.get("/getInventory",inController.getInventory)
+inRoute.put('/updateInventory',inController.updateInventory)
+inRoute.delete("/deleteInventory",inController.deleteInventory)
