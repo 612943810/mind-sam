0cdc2ec6256b97273fda42e7b40fee5ccbcb11d3
612943810 <raiyanrofiquizzaman@gmail.com>
2023-12-25 23:43:39 -0500

    This update fixes register model name.

--- mind-sam-backend/ts/models/RegisterModel..ts
+++ mind-sam-backend/ts/models/RegisterModel..ts
@@ -1,15 +1,15 @@
 import mongoose, { Schema, model } from "mongoose";
 
-export type Register={
+export type RegisteredUser={
 username:string
 password:string
 dateofbirth:Date
 }
 
-let registerSchema=new Schema<Register>({
+let registerSchema=new Schema<RegisteredUser>({
     username:{type:String,required:true},
   password:{type:String,required:true},
   dateofbirth:{type:Date,required:true}
 })
 
-export let inventory=model<Register>('Users',registerSchema);
+export let inventory=model<RegisteredUser>('Users',registerSchema);
