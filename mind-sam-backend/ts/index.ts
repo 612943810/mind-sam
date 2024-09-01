1c0936d8bb55ed3e149967cafa435e323a4bf35a
612943810 <raiyanrofiquizzaman@gmail.com>
2024-08-31 23:42:40 -0400

    This update fixes code.

--- mind-sam-backend/ts/index.ts
+++ mind-sam-backend/ts/index.ts
@@ -11,7 +11,6 @@ import { authRoute } from './routes/authentication';
 const cookieParser = require('cookie-parser')
 import fs from 'fs';
 import path from 'path';
-import session from  'express-session';
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.database_name}:${process.env.database_password}@${process.env.database_name}.yhrxz.mongodb.net/inventory?retryWrites=true&w=majority`);
