7d19a12bb8a9a22ca1770c530c8d31961c782c8c
612943810 <raiyanrofiquizzaman@gmail.com>
2024-08-31 22:56:44 -0400

    This update adds logout button to nav on username.

--- mind-sam-frontend/src/navigation/Navigation.tsx
+++ mind-sam-frontend/src/navigation/Navigation.tsx
@@ -1,41 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './Navigation.css';
 import { useEffect, useState } from 'react';
 export default function Navigation() {
-  let currentParams=new URLSearchParams(window.location.search)
-  let currentUser=currentParams.get('username')
-  useEffect(()=>{
+  let currentParams = useParams()
+  let currentUser = currentParams.username
+  useEffect(() => {
     //if()
   })
-  return ( 
+  return (
     <>
 
-         <nav  className='navigationBar'>
-  {
-    currentUser?
-<Link  to={`/inventory?username=${currentUser}`}className='navLink'> Inventory</Link>
-  :  
-   <>
-    <Link  to={`/inventory`}className='navLink'> Inventory</Link>
-    </>
-  }
-       
-       {
-    currentUser?
-   ''
-:  <>
-    <li className=' navLink loginPosition'>
-            <a  className='registerPad'href='/register'>Register</a>
-       </li>
-          <li className=' navLink '>
-            <a href='/login'>Login</a>
-          </li>
-  
-    </> }
-          
+      <nav className='navigationBar'>
+        {
+          currentUser ?
+            <>
+              <Link to={`/inventory/${currentUser}`} className='navLink'> Inventory</Link>
+              <Link to='' className='navLink'>Logout</Link>
+            </>
+
+            :
+            <>
+              <Link to={`/inventory`} className='navLink'> Inventory</Link>
+
+            </>
+        }
+
+        {
+          currentUser ?
+            ''
+            : <>
+              <li className=' navLink loginPosition'>
+                <a className='registerPad' href='/register'>Register</a>
+              </li>
+              <li className=' navLink '>
+                <a href='/login'>Login</a>
+              </li>
+
+            </>}
+
       </nav>
 
-   
+
     </>
 
   )
